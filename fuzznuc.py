572acd7dda78812ddab5e7c8041dfa483f3ec0cb
pkoukos <pkoukos2009@hotmail.com>
2015-06-22 11:51:20 +0100

    write the call_fuzznuc prototype

--- fuzznuc.py
+++ fuzznuc.py
@@ -19,15 +19,9 @@ nucleotide sequence and stores the results in an aptly named text file.
 import subprocess
 # import sys
 
-# From http://stackoverflow.com/a/11270665
-try:
-    from subprocess import DEVNULL  # py3k
-except ImportError:
-    import os
-    DEVNULL = open(os.devnull, 'wb')
-
 
 def check_fuzznuc():
+    # TODO Port this through python instead of which for compatibility?
     """
     Check for the presence of fuzznuc in the local system.
 
@@ -38,6 +32,12 @@ def check_fuzznuc():
     :return: The absolute path to the fuzznuc program as returned
              by which on success, None on failure.
     """
+    # From http://stackoverflow.com/a/11270665
+    try:
+        from subprocess import DEVNULL  # py3k
+    except ImportError:
+        import os
+        DEVNULL = open(os.devnull, 'wb')
 
     try:
         # stdout=DEVNULL suppresses the output of the system call.
@@ -59,13 +59,41 @@ def call_fuzznuc(fuzznuc, input_file, output_file, pattern_file):
     :param fuzznuc: Full path to the fuzznuc binary, as found by check_fuzznuc.
     :type fuzznuc: str or unicode
     :param input_file: Path to the sequence (FASTA) file.
-    :type fuzznuc: str or unicode
+    :type input_file: str or unicode
     :param output_file: File in which fuzznuc will store its output.
-    :type fuzznuc: str or unicode
+    :type output_file: str or unicode
     :param pattern_file: Path to the file which contains the fuzznuc settings.
-    :type fuzznuc: str or unicode
+    :type pattern_file: str or unicode
     :return: None
     """
+    # From http://stackoverflow.com/a/11270665
+    # TODO fix the repeating devnull import. Maybe use an exit function?
+    try:
+        from subprocess import DEVNULL  # py3k
+    except ImportError:
+        import os
+        DEVNULL = open(os.devnull, 'wb')
 
+    if not output_file:
+        output_file = input_file.split('.')[0] + '.fuzznuc'
+        # print output_file
+
+    pattern_file = '@' + pattern_file
+    # print pattern_file
+
+    try:
+        subprocess.check_call([fuzznuc,
+                               '-sequence', input_file,
+                               '-pattern', pattern_file,
+                               '-outfile', output_file],
+                              stdout=DEVNULL,
+                              stderr=subprocess.STDOUT)
+    except subprocess.CalledProcessError:
+        print 'One or more files not found. Aborting.'
+        return None
 
 # print check_fuzznuc()
+call_fuzznuc(check_fuzznuc(), 'ERR145618_1.FASA', None, 'nuc.pat')
+
+# TODO: Write the __name__ == main wrapper so that this is usable as a module
+# TODO: and a script too.
