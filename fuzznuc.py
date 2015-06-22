72a08b6332dc7d5e2efcd41baf9af9b56115bcd7
pkoukos <pkoukos2009@hotmail.com>
2015-06-22 10:10:42 +0100

    add the options comments to the fuzznuc file

---
+++ fuzznuc.py
@@ -0,0 +1,17 @@
+__author__ = 'Panagiotis Koukos'
+
+"""
+fuzznuc.py - Scan input files with fuzznuc.
+
+This module scans the input files for the presence of the specified
+nucleotide sequence.
+
+:param -i, --in: Path to the file which contains the sequences to parse.
+:type -i, --in: str or unicode
+:param -p, --pat: Path to the file which contains the pattern to use.
+:type -p, --pat: str or unicode
+:param -o, --out: What file to write the results in.
+:type -o, --out: str or unicode
+:return None
+"""
+
