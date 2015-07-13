36966531e2dcd966736c7158aee91cda862449bc
pkoukos <pkoukos2009@hotmail.com>
2015-07-13 12:40:20 +0100

    add argument parsing for the sequence extraction. also a couple of things for the 2DO list in the README.

--- README.md
+++ README.md
@@ -17,3 +17,11 @@ A python pipeline for the detection of HERV-K elements in modern and ancient Hom
 2. add the subprocess.CalledProcessError to the except maybe
 3. separate the three stages(run fuzznuc, check results, extract) in three
    functions
+
+## Write a couple of classes to generalise some things:
+
+- A class to read a directory and return all the contents.
+  - Filter the results in the class as an additional argument.
+  - Do no t filter anything and do the filtering in the calling part of the code.
+
+- A class for the read processing? Though classes cannot be dumped in JSON objects.
--- extract_sequences.py
+++ extract_sequences.py
@@ -1,9 +1,44 @@
 __author__ = 'Panagiotis Koukos'
 
+"""
+This module extracts the sequences from the original input files based
+on the findings of the previous two scripts in the pipeline.
+"""
+import json
+import os
+
+
+def load_json(path_to_json_dir):
+    json_files = []
+    for _file in os.listdir(path_to_json_dir):
+        if _file.endswith('json'):
+            with open(_file) as in_file:
+                json_files.append(json.load(in_file))
+    return json_files
+
 
 def main():
-    import os
-    print os.listdir('.')
+    import argparse
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-fasta',
+                        '--fasta_input_dir',
+                        type=str,
+                        required=False,
+                        help='Input directory. This is the directory where the FASTA'
+                             ' files are located. Defaults to the current working directory.')
+    parser.add_argument('-json',
+                        '--json_input_dir',
+                        type=str,
+                        required=False,
+                        help='Input directory. This is the directory where the JSON'
+                             ' files are located. Defaults to the current working directory.')
+
+    args = parser.parse_args()
+    fasta_dir, json_dir = args.fasta_input_dir, args.json_input_dir
+
+    json_files = load_json(json_dir)
+    for json_file in json_files:
+        print json_file, json_file.keys()
 
 if __name__ == '__main__':
     main()
