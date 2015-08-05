93d957c5f9ed4c056b909bd5d667da691ecbc4a8
pkoukos <pkoukos2009@hotmail.com>
2015-08-05 20:26:23 +0100

    initial commit. file does nothing.

---
+++ process_blast_output.py
@@ -0,0 +1,13 @@
+__author__ = 'Panagiotis Koukos'
+
+"""
+This module processes the blast output in order to extract the useful
+information and store it in an easily accessible format.
+"""
+
+
+def main():
+    return
+
+if __name__ == '__main__':
+    main()
