fdc24221ed034d07ef18c6f427f776dae3e8ec68
pkoukos <pkoukos2009@hotmail.com>
2015-09-10 16:35:07 +0100

    Cleanup of the pipeline:
    1. Send the raw sequences to fuzznuc instead of 3 and 5 prime.
    2. Get rif of teh call_fuzznuc function. It was useless.
    3. Check for the existence of fasta files and fuzznuc files before their respective loops.
    4. Move the module imports at the module level.

--- pipeline.py
+++ pipeline.py
@@ -6,30 +6,18 @@ for the analysis in one place. It imports/launches the other scripts of the
 pipeline and executing them with their default options.
 
 """
+import sys
 import herv_lib
 import run_fuzznuc
-
-
-def call_fuzznuc(fuzznuc, list_of_fasta_files):
-    for fasta_file in list_of_fasta_files:
-        for prime in ['5_prime', '3_prime']:
-            try:
-                run_fuzznuc.call_fuzznuc(fuzznuc.path,  # Path to the executable
-                                         fasta_file,    # Input file
-                                         '',            # Output file-name
-                                         prime,         # 3 or 5 prime
-                                         '2')           # Number of mismatches
-            except ValueError as e:
-                print e
+import subprocess
+import process_results
+import create_fasta_from_json
+import extract_sequences
+import process_blast_output
 
 
 def main():
     import argparse
-    from subprocess import call
-    import process_results as pr
-    import create_fasta_from_json as cf
-    import extract_sequences as ec
-    import process_blast_output as pb
 
     fuzznuc = herv_lib.Executable('fuzznuc')
 
@@ -48,33 +36,46 @@ def main():
         curr_dir = herv_lib.Directory()  # Default is the current directory.
 
         list_of_fasta_files = in_dir.get_files_with_suffix('FASTA')
-        print list_of_fasta_files
-        call_fuzznuc(fuzznuc, list_of_fasta_files)
+        if not list_of_fasta_files:
+            sys.exit('No *.FASTA files found in the specified folder. Aborting.')
 
-        # Update the contents of the directory after fuzznuc is done.
+        # First parse the file with fuzznuc looking for the 5prime sequence and
+        # then for the 3prime. Keep the loop structure like this because of disk
+        # buffering.
+        for fasta_file in list_of_fasta_files:
+            for ltr_seq in ['TGTGGGGAAAAGCAAGAGAG', 'AGGGGCAACCCACCCCTACA']:
+                run_fuzznuc.call_fuzznuc(fuzznuc.path,  # Path to the executable
+                                         fasta_file,    # Input file
+                                         '',            # Output file-name
+                                         ltr_seq,       # 3 or 5 prime
+                                         '2',)          # Number of mismatches
+
+        # Update the contents of the directory after fuzznuc is done so that the
+        # fuzznuc files are added to the contents attribute.
         curr_dir.set_dir_contents()
 
         list_of_fuzznuc_files = curr_dir.get_files_with_suffix('fuzznuc')
-        print list_of_fuzznuc_files
-        for fuzznuc_file in list_of_fuzznuc_files:
-            results = pr.parse_report(fuzznuc_file)
-            pr.write_json(results)
+        if not list_of_fuzznuc_files:
+            sys.exit('No *.fuzznuc files found in the specified folder. Aborting.')
 
-        call(['python',
-              'extract_sequences.py',
-              '-fasta',
-              in_dir.path])
+        for fuzznuc_file in list_of_fuzznuc_files:
+            results = process_results.parse_report(fuzznuc_file)
+            process_results.write_json(results)
+
+        subprocess.call(['python',
+                         'extract_sequences.py',
+                         '-fasta',
+                         in_dir.path])
 
         # Create the extract_sequences.fa file.
         curr_dir.set_dir_contents()
         list_of_json_files = curr_dir.get_files_with_suffix('json')
-        if len(list_of_json_files) < 1:
-            from sys import exit
-            exit('No json files found in the specified directory. Aborting.')
+        if not list_of_json_files:
+            sys.exit('No json files found in the specified directory. Aborting.')
 
-        json_files = ec.load_json(list_of_json_files)
-        processed_json = cf.extract_from_json(json_files)
-        cf.write_to_file(processed_json)
+        json_files = extract_sequences.load_json(list_of_json_files)
+        processed_json = create_fasta_from_json.extract_from_json(json_files)
+        create_fasta_from_json.write_to_file(processed_json)
 
         # Run blast.
         # call([
@@ -92,8 +93,8 @@ def main():
 
         # Filter out the blast hits in the repetitive regions. Requires the file
         # from the previous step in JSON format.
-        repeats = pb.load_repeating_regions('repeating_regions.out')
-        pb.process_blast_output('sample_blast.out', repeats)
+        # repeats = process_blast_output.load_repeating_regions('repeating_regions.out')
+        # process_blast_output.process_blast_output('sample_blast.out', repeats)
         # pb.write_valid_hits(blast_hits)
     else:
         print 'Failed to locate fuzznuc binary in the system path. Aborting.'
